129be4c4c0b08aebeeaecafd143f7cbfccc79c8e
karthik-kosuri <kosurikarthik07@gmail.com>
2025-01-05 12:58:35 +0530

    updated app.js with changes

--- backend/app.js
+++ backend/app.js
@@ -16,5 +16,5 @@ app.use("/api", authRoutes);
 
 // Start Server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port  http://localhost:${PORT}`);
 });
